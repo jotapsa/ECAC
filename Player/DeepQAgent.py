f23505ab4159295339ea678b2e36e20d033c802b
João Sá <up201506252@fe.up.pt>
2021-01-03 22:03:03 +0000

    Bunch of improvements to the Deep Q Network

--- Player/DeepQAgent.py
+++ Player/DeepQAgent.py
@@ -13,14 +13,14 @@ import torchvision.transforms as T
 from Player.Api.Agent import Agent
 
 BATCH_SIZE = 64
-GAMMA = 0.99            # discount factor
-LR = 5e-4               # learning rate
+GAMMA = 0.99  # discount factor
+TAU = 1e-3  # for soft update of target parameters
+LR = 5e-4  # learning rate
 EPS_START = 0.9
 EPS_END = 0.05
 EPS_DECAY = 200
-UPDATE_EVERY = 4        # how often to update the network
+UPDATE_EVERY = 4  # how often to update the network
 TARGET_UPDATE = 10
-
 
 # if gpu is to be used
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -30,7 +30,6 @@ Transition = namedtuple('Transition',
 
 
 class ReplayMemory(object):
-
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
@@ -58,9 +57,6 @@ class DeepQNetwork(torch.nn.Module):
         self.f2 = nn.Linear(fc1_nodes, fc2_nodes)
         self.f3 = nn.Linear(fc2_nodes, action_size)
 
-        self.optimizer = optim.RMSprop(self.parameters(), lr=LR)
-        self.loss = nn.MSELoss()
-
     def forward(self, state):
         output = F.relu(self.f1(state))
         output = F.relu(self.f2(output))
@@ -82,8 +78,12 @@ class DeepQAgent(Agent):
             action_size=action_size
         ).to(device)
 
-        # self.target_net.load_state_dict(self.policy_net.state_dict())
-        # self.target_net.eval()
+        self.policy_net.eval()
+
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.target_net.eval()
+
+        self.optimizer = optim.RMSprop(self.policy_net.parameters(), lr=LR)
 
         # Replay memory
         self.memory = ReplayMemory(10000)
@@ -94,18 +94,13 @@ class DeepQAgent(Agent):
         pass
 
     def update(self, s0, s1, a, r, t):
-        # self.memory.push([float(s0)], a, [float(s1)], r, t)
         self.memory.push(s0, a, s1, r, t)
         self.learn()
         pass
 
     def get_action(self, observation):
         eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * self.steps_done / EPS_DECAY)
-
-        # self.qnetwork_local.eval()
-        # with torch.no_grad():
-        #     action_values = self.qnetwork_local(state)
-        # # self.qnetwork_local.train()
+        self.policy_net.eval()
 
         self.steps_done += 1
         if random.random() > eps_threshold:
@@ -133,25 +128,42 @@ class DeepQAgent(Agent):
         # to Transition of batch-arrays.
         batch = Transition(*zip(*transitions))
 
-        batch_index = np.arange(BATCH_SIZE, dtype=np.int64)
-
         state_batch = torch.tensor(batch.state).float().reshape(BATCH_SIZE, 1).to(device)
-        action_batch = torch.tensor(batch.action).to(device)
+        action_batch = torch.tensor(batch.action).long().reshape(BATCH_SIZE, 1).to(device)
         next_state_batch = torch.tensor(batch.next_state).float().reshape(BATCH_SIZE, 1).to(device)
-        reward_batch = torch.tensor(batch.reward).to(device)
+        reward_batch = torch.tensor(batch.reward).unsqueeze(1).to(device)
         terminal_batch = torch.tensor(batch.terminal).to(device)
 
-        state_action_values = self.policy_net.forward(state_batch)
-        next_state_values = self.target_net.forward(next_state_batch)
+        criterion = nn.MSELoss()
+
+        self.policy_net.train()
+
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+        with torch.no_grad():
+            next_state_values = self.target_net(next_state_batch).detach().max(1)[0].unsqueeze(1)
+
         next_state_values[terminal_batch] = 0.0
 
-        reward_batch = reward_batch.reshape(BATCH_SIZE, 1)
-        reward_batch = reward_batch.expand(-1, self.action_size)
         expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
-        # # Compute loss
-        loss = self.policy_net.loss(state_action_values, expected_state_action_values).to(device)
+        # Compute loss
+        loss = criterion(state_action_values, expected_state_action_values).to(device)
         # Optimize the model
-        self.policy_net.optimizer.zero_grad()
-        # loss.backward()
-        self.policy_net.optimizer.step()
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        self.soft_update(self.policy_net, self.target_net, TAU)
+
+    def soft_update(self, local_model, target_model, tau):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+        Params
+        =======
+            local model (PyTorch model): weights will be copied from
+            target model (PyTorch model): weights will be copied to
+            tau (float): interpolation parameter
+        """
+        for target_param, local_param in zip(target_model.parameters(),
+                                             local_model.parameters()):
+            target_param.data.copy_(tau * local_param.data + (1 - tau) * target_param.data)
